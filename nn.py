0de97d000199368ef040c2a74d0e3655c3818e55
Alex Shrestha <alexshrestha1996@gmail.com>
2024-08-12 11:41:15 -0700

    neural network implementation

--- nn.py
+++ nn.py
@@ -1,8 +1,26 @@
-import torch.nn as nn
+import torch
 import torch.nn.functional as F
 
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor
 
-class MNISTNeural(nn.Module):
+training_data = datasets.MNIST(
+        root="data",
+        train=True,
+        download=True,
+        transform=ToTensor()
+)
+
+test_data = datasets.MNIST(
+        root="data",
+        train=False,
+        download=True,
+        tranform=ToTensor()
+)
+
+class NeuralNetwork(nn.Module):
     def __init__(self):
         super().__init__()
 
@@ -21,16 +39,78 @@ class MNISTNeural(nn.Module):
                 nn.Linear(50, 10),
         )
 
+
     def forward(self, x):
         # Might not need to flatten data depending on how we are ingesting each picture
         x = self.flatten(x)
         logits = self.linear_relu_stack(x)
 
+        return logits
+
+        """
         # Using softmax to predict digit
         pred_probab = nn.Softmax(dim=1)(logits)
         y_pred = pred_probab.argmax(1)
 
         return y_pred
+        """
 
 
+class MNISTNeural():
+    def __init__(self, learning_rate=0, batches=1, epochs=10):
+        self.model = NeuralNetwork()
+        self.epochs = epochs
+        self.batch_size = batches
+
+        self.train_dataloader = DataLoader(training_data, batch_size=batches)
+        self.test_dataloader = DataLoader(test_data, batch_size=batches)
+
+        self.loss_fn = nn.CrossEntropyLoss()
+        self.optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+        
+    def train(self): 
+        size = len(self.train_dataloader.dataset)
+        self.model.train()
+
+        for batch, (X, y) in enumerate(self.train_dataloader):
+            pred = self.model(X)
+            loss = loss_fn(pred, y)
+
+            # Backpropagation
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+
+            if batch % 100 == 0:
+                loss, current = loss.item(), batch * self.batch_size + len(X) 
+                print(f"loss: {loss:>7f} [{current:>5d}/{size:>5d}]")
+
+
+    def test(self):
+        self.model.eval()
+
+        size = len(self.test_dataloader.dataset)
+        num_batches = len(self.test_dataloader)
+        test_loss, correct = 0, 0
+
+
+        with torch.no_grad():
+            for X, y in self.test_dataloader:
+                pred = model(X)
+                test_loss += loss_fn(pred, y).item()
+                correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+            
+        test_loss /= num_batches
+        correct /= size
+
+        print(f"Test Error: \n Accuracy: {(100 * correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
+
+    def run(self):
+        for t in range(self.epochs):
+            print(f"Epoch {t+1}\n-----------------------------------")
+            self.train()
+            self.test()
+
+        print("Finished")
 
