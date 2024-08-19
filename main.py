34e03ccc0313c54cd93e53c95d52d5b6e7ffa90d
Will Schweitzer <willSchweitzer@yahoo.com>
2024-08-18 19:37:37 -0700

    Implemented train() and test() for the SVM.  Created a file util for parse_data_line()

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 # import nn
+import numpy as np
 from svm import SVM
-from kernel_functions import kernel_and
+
 
 def main():
+    training_data = np.genfromtxt("mnist_train.csv", dtype=int, delimiter=",", skip_header=59500)
+    testing_data = np.genfromtxt("mnist_test.csv", dtype=int, delimiter=",", skip_header=1)
+
+    
     # model = nn.MNISTNeural(learning_rate=0.01, batches=64, epochs=20)
     # model.run()
 
-    svm = SVM((1, 1), kernel_and)
 
+    svm = SVM(num_classes=10, kernel="linear")
+    svm.train(training_data)
+    svm.test(testing_data)
 
+    print(svm.accuracy)
+    print(svm)
 
 
 if __name__ == "__main__":
--- svm.py
+++ svm.py
@@ -1,15 +1,17 @@
-''' Imports '''
-from typing import Tuple, Callable
-from confusion_matrix import ConfusionMatrix
+''' SVM class '''
+from typing import Callable
 from sklearn import svm
+from confusion_matrix import ConfusionMatrix
+from utils import parse_data_line
 
+NORMALIZATION = 255.0
 
 class SVM:
     '''
     SVM class
     '''
-    def __init__(self, size: Tuple[int, int], kernel: Callable[..., float], shift: float = 1.0):
-        self._confusion_matrix = ConfusionMatrix(size=size)
+    def __init__(self, num_classes: int, kernel: Callable[..., float], shift: float = 1.0):
+        self._confusion_matrix = ConfusionMatrix(size=(num_classes, num_classes))
         self._kernel = kernel
         self._shift = shift
         self._classifier = svm.SVC(kernel=self._kernel, C=self._shift)
@@ -42,16 +44,28 @@ class SVM:
         '''
         return
 
-    def train (self, input_data):
+    def train(self, input_data):
         '''
         train
         '''
-        targets = np.array(input_data[:, -1])
-        input_data = input_data[:, :-1]
-        return self._classifier.fit(input_data, targets)
+        # Extract targets and features from the input data.  Normalize the features.
+        targets, features = parse_data_line(input_data, normalization=NORMALIZATION)
+
+        # Train the classifier
+        return self._classifier.fit(features, targets)
 
     def test (self, test_data) -> None:
         '''
         test
         '''
-        return
+
+        # Extract targets and features from the input data.  Normalize the features.
+        targets, features = parse_data_line(test_data, normalization=NORMALIZATION)
+
+        classifications = self._classifier.predict(features)
+
+        # Populate the confusion matrix
+        for target, prediction in zip(targets, classifications):
+            self._confusion_matrix[target][prediction] += 1
+
+        return self._confusion_matrix.accuracy
